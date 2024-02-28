d1ab64f6c4056d6a799c120da85b05b9fe5861c4
bereket-7 <bekhonelign@gmail.com>
2024-02-28 05:04:35 +0300

    budget.py

---
+++ model/budget.py
@@ -0,0 +1,7 @@
+class budget:
+    def __init__(self,id,amount,category,start_date,end_date):
+        self.id = id
+        self.amount = amount
+        self.category = category
+        self.start_date = start_date
+        self.end_date = end_date
