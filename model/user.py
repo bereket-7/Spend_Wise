2050f1473acdbecb6e58f922fc442b80aea04a51
bereket-7 <bekhonelign@gmail.com>
2024-02-28 04:58:45 +0300

    user.py

---
+++ model/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, user_id, username, password, email,phone_number,first_name,last_name):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.email = email,
+        self.phone_number = phone_number,
+        self.first_name = first_name,
+        self.last_name = last_name
