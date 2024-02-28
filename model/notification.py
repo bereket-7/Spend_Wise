4125532284ef9633c7537f2f618ed456c04cf03d
bereket-7 <bekhonelign@gmail.com>
2024-02-28 05:09:07 +0300

    notification.py

---
+++ model/notification.py
@@ -0,0 +1,8 @@
+class notification:
+    def __init__(self, id,notification_type,enabled, message,sent,time):
+        self.id = id,
+        self.notification_type = notification_type,
+        self.enabled = enabled,
+        self.message = message,
+        self.sent = sent,
+        self.time = time
