9ca74d62791d844ca0866f976b4a1235e0b7a0cb
bereket-7 <bekhonelign@gmail.com>
2024-03-02 03:04:07 +0300

    initiakl calculator service

---
+++ utils/calculator_service.py
@@ -0,0 +1,18 @@
+class CalculatorService:
+    @staticmethod
+    def add(x, y):
+        return x + y
+
+    @staticmethod
+    def subtract(x, y):
+        return x - y
+
+    @staticmethod
+    def multiply(x, y):
+        return x * y
+
+    @staticmethod
+    def divide(x, y):
+        if y == 0:
+            raise ValueError("Division by zero is not allowed")
+        return x / y
