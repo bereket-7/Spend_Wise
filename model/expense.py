164df9fd4b07996207114fbf65d86423b83fce93
bereket-7 <bekhonelign@gmail.com>
2026-02-21 12:55:56 +0300

    expense

--- model/expense.py
+++ model/expense.py
@@ -1,6 +1,6 @@
-class expense :
-    def __init__(self, id, amount,category,date):
-        self.id = id,
-        self.amount = amount,
-        self.category = category,
+class expense:
+    def __init__(self, id: int, amount: float, category: str, date: str):
+        self.id = id
+        self.amount = amount
+        self.category = category
         self.date = date
