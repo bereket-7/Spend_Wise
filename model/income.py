d1027bea194a7b0213b8e8dee2b4d9b82ba6a9f3
bereket-7 <bekhonelign@gmail.com>
2026-02-21 12:56:04 +0300

    income

--- model/income.py
+++ model/income.py
@@ -1,6 +1,11 @@
+from typing import Optional
+from datetime import datetime
+
 class income:
-    def __init__(self,id,amount,source,date):
+    def __init__(self, id: Optional[int], amount: float, source: str, date: str, user_id: int):
         self.id = id
         self.amount = amount
         self.source = source
         self.date = date
+        self.user_id = user_id
+        self.created_at = datetime.now().isoformat()
