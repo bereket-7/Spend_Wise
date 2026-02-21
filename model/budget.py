6fc143ede725f200e74c031f3bf2520aa46bbe07
bereket-7 <bekhonelign@gmail.com>
2026-02-21 12:55:45 +0300

    budget

--- model/budget.py
+++ model/budget.py
@@ -1,7 +1,12 @@
+from typing import Optional
+from datetime import datetime
+
 class budget:
-    def __init__(self,id,amount,category,start_date,end_date):
+    def __init__(self, id: Optional[int], amount: float, category: str, start_date: str, end_date: str, user_id: int):
         self.id = id
         self.amount = amount
         self.category = category
         self.start_date = start_date
         self.end_date = end_date
+        self.user_id = user_id
+        self.created_at = datetime.now().isoformat()
